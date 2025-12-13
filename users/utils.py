40bf64726f393f385d0b5d2ff4ff4a62710d970c
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-13 21:12:44 +0330

    Add resend_varification_email

--- users/utils.py
+++ users/utils.py
@@ -29,3 +29,16 @@ def store_email_resend_varification_token(user):
     token=RefreshToken.for_user(user)
     token['resend_email_verify']=True
     return str(token)
+
+def resend_varification_email(user):
+    token=store_email_resend_varification_token(user)
+    verification_link = (
+        f"http://localhost:8000/users/api/v1/resend/activation/?token={token}"    )
+
+    send_mail(
+        subject='Resend Activate your account',
+        message=f'Click this link to verify your email: {verification_link}',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.email],
+        fail_silently=False
+    )
