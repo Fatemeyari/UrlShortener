e9d5c48d32e962e6afb514c4e1c731c5194d1faf
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-14 19:18:56 +0330

    Add UserLoginSerializer

--- users/api/v1/serializers.py
+++ users/api/v1/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
+
 from ...models import User
 
 
@@ -29,3 +31,23 @@ class ResendActivationsSerializer(serializers.Serializer):
         if not User.objects.filter(email=value).exists():
             raise serializers.ValidationError("User with this email does not exist.")
         return value
+
+
+class UserLoginSerializer(serializers.Serializer):
+    phone_number=serializers.CharField(max_length=13)
+    password=serializers.CharField(label='Password')
+
+    def validate(self , attrs):
+        phone_number=attrs['phone_number']
+        password=attrs['password']
+        if phone_number and password:
+            user=authenticate(phone_number=phone_number,password=password)
+            if not user:
+                raise serializers.ValidationError("Invalid phone number or password.")
+            if not user.is_active:
+                raise serializers.ValidationError("User account is not active.")
+        else:
+            raise serializers.ValidationError("Both phone number and password are required.")
+
+        attrs["user"] = user
+        return attrs
