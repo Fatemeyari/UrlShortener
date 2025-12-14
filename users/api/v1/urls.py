15fe4142b5081ca825ba46f84ab3e8cceed65463
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-14 19:29:44 +0330

    Add UserLoginAPIView

--- users/api/v1/urls.py
+++ users/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserRegistrationAPIView , VerifyEmailAPIView , ResendActivationAPIView , ResendVerifyEmailAPIView
+from .views import (UserRegistrationAPIView , VerifyEmailAPIView , ResendActivationAPIView , ResendVerifyEmailAPIView,
+                    UserLoginAPIView)
 
 urlpatterns = [
     path('registration/' , UserRegistrationAPIView.as_view() , name='user-registration'),
     path('activation/', VerifyEmailAPIView.as_view(), name='user-activation'),
     path('resend-registration/', ResendActivationAPIView.as_view(), name='resend-user-registration'),
-    path('resend/activation/' ,ResendVerifyEmailAPIView.as_view() , name='resend-user-activation')
-]
+    path('resend/activation/' ,ResendVerifyEmailAPIView.as_view() , name='resend-user-activation'),
+    path('login/', UserLoginAPIView.as_view(), name='user-login'),]
--- users/api/v1/views.py
+++ users/api/v1/views.py
@@ -6,7 +6,8 @@ from rest_framework import status
 from rest_framework_simplejwt.tokens import AccessToken , RefreshToken
 from django.contrib.auth import get_user_model
 
-from .serializers import UserRegistrationSerializer , ResendActivationsSerializer
+from .serializers import UserRegistrationSerializer , ResendActivationsSerializer , UserLoginSerializer
+
 from ...utils import send_verification_email , resend_varification_email
 
 User = get_user_model()
@@ -102,3 +103,20 @@ class ResendVerifyEmailAPIView(APIView):
 
         except Exception as e:
             return Response({"detail": "Invalid or expired token"}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginAPIView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+    def post(self , request):
+        serializer=self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user=serializer.validated_data.get("user")
+        refresh = RefreshToken.for_user(user)
+        data = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token)
+        }
+
+        return Response({
+            "message": "Login successful",
+            "tokens": data
+        }, status=status.HTTP_200_OK)
