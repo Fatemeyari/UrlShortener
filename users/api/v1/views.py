df9aa2a91190e572b2fc0c9620044942c07f602d
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-14 23:11:08 +0330

    Create endpoint for resetting password using token

--- users/api/v1/views.py
+++ users/api/v1/views.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.tokens import AccessToken , RefreshToken , TokenEr
 from django.contrib.auth import get_user_model
 
 from .serializers import (UserRegistrationSerializer , ResendActivationsSerializer , UserLoginSerializer ,
-                          PasswordResetRequestSerializer )
+                          PasswordResetRequestSerializer ,PasswordResetConfirmSerializer)
 from ...utils import send_verification_email , resend_varification_email , send_password_reset_email
 
 User = get_user_model()
@@ -149,3 +149,26 @@ class PasswordResetRequestAPIView(generics.GenericAPIView):
         send_password_reset_email(user)
         return Response({"message": "Password reset link has been sent to your email."}, status=status.HTTP_200_OK)
 
+
+class PasswordResetConfirmAPIView(APIView):
+    serializer_class = PasswordResetConfirmSerializer
+
+    def post(self, request):
+        token = request.query_params.get('token')
+        if not token:
+            return Response({"detail": "Token missing."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            payload = AccessToken(token)
+            if not payload.get('reset_password'):
+                return Response({"detail": "Invalid reset token."}, status=status.HTTP_400_BAD_REQUEST)
+            user_id = payload['user_id']
+            user = User.objects.get(id=user_id)
+        except Exception:
+            return Response({"detail": "Invalid or expired token."}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user.set_password(serializer.validated_data['new_password'])
+        user.save()
+        return Response({"message": "Password has been reset successfully."}, status=status.HTTP_200_OK)
