01d50a7795dbaf7e3ae039e50013fcd30606b6cc
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-15 21:28:16 +0330

    0001_initial.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'phonenumber_field',
-    'users'
+    'users',
+    'Linko'
 ]
 
 MIDDLEWARE = [
