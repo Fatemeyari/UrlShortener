b390a6ea2af082367b2c76bfb0c89d250e963994
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-16 14:50:57 +0330

    improve ShortURL admin configuration

--- Linko/admin.py
+++ Linko/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ShortURL
+
+@admin.register(ShortURL)
+class ShortUrlAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_time'
+    list_display = [
+        'show_user', 'show_email' , 'original_url' , 'short_code' , 'is_active'
+    ]
+
+    list_display_links = ('short_code',)
+
+    fieldsets = [
+        ('Information of Link', {
+            'fields': ('user', 'original_url', 'short_code','clicks', 'is_active', 'custom_alias', 'qr_code')
+        }),
+        ('Date Information' , {
+            'fields': ('created_time','expires_time','last_clicked')
+        })
+
+    ]
+    readonly_fields = ['created_time', 'clicks', 'last_clicked']
+    list_filter = ['user','is_active' , 'created_time','expires_time']
+    search_fields = ['original_url', 'user__email', 'user__phone_number']
+    empty_value_display='empty'
+
+    @admin.display(description='Phone Number')
+    def show_user(self , obj):
+        if hasattr(obj , 'user') and obj.user:
+            return obj.user.phone_number
+        return '-'
+
+    @admin.display(description='Email')
+    def show_email(self , obj):
+        if hasattr(obj , 'user') and obj.user:
+            return obj.user.email
+        return '-'
+
