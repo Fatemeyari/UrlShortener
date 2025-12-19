be610c62d0c1d9f273a21a56ceff185aab85922e
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-19 14:31:10 +0330

    add service to generate unique short_code

---
+++ links/short_code.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+from .models import ShortURL
+
+ALIAS_RESERVED =['admin' , 'api']
+
+def generate_short_code(length=6):
+    chars=string.ascii_letters+string.digits
+    while True:
+        code=''.join(random.choices(chars, k=length))
+        if not ShortURL.objects.filter(short_code=code).exists():
+            return code
