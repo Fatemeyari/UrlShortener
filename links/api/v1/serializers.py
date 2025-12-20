4b529e6c175fb065bf176d9e8d8564ecb59545d8
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-20 15:49:22 +0330

    fix ShortUrlCreateSerializer

--- links/api/v1/serializers.py
+++ links/api/v1/serializers.py
@@ -11,12 +11,13 @@ class ShortUrlCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user=self.context['request'].user
-        if not user.is_premium:
-            if ShortURL.objects.filter(user=user).count() >=50:
+        if not user.profile.is_premium:
+            user_links_count = ShortURL.objects.filter(user=user).count()
+            if user_links_count >= 50:
                 raise serializers.ValidationError(
                     "Non-premium users can only create up to 50 links."
                 )
-        custom_alias = validated_data.get('custom_alias' , None)
+        custom_alias = validated_data.get('custom_alias')
         if custom_alias:
             validate_short_url(custom_alias)
             short_code = custom_alias
