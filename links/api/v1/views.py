86cb69849d776c471bd2df611d227d4148702a45
Fatemeyari <fatemeyarii82@gmail.com>
2025-12-20 13:49:59 +0330

    Add ShortUrlUpdatedAPIView

--- links/api/v1/views.py
+++ links/api/v1/views.py
@@ -1,8 +1,19 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import ShortUrlCreateSerializer
+
+from .serializers import ShortUrlCreateSerializer , ShortUrlUpdateSerializer
+from ...models import ShortURL
+
 
 class ShortUrlCreateAPIView(CreateAPIView):
     serializer_class = ShortUrlCreateSerializer
     permission_classes = [IsAuthenticated]
+
+
+class ShortUrlUpdatedAPIView(UpdateAPIView):
+    serializer_class = ShortUrlUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return ShortURL.objects.filter(user=self.request.user)
