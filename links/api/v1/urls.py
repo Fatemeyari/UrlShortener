48006ff229a06fe8afa4be32cbcdd2e40b791687
Fatemeyari <fatemeyarii82@gmail.com>
2026-01-03 19:40:31 +0330

    feat: add Redis-based redirect for short URLs

--- links/api/v1/urls.py
+++ links/api/v1/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import ShortUrlCreateAPIView , ShortUrlUpdatedAPIView,ShortUrlDeleteAPIView,ShortUrlListAPIView
+from .views import ShortUrlCreateAPIView , ShortUrlUpdatedAPIView,ShortUrlDeleteAPIView,ShortUrlListAPIView,ShortURLRedirectAPIView
 
 urlpatterns = [
     path('create-url/', ShortUrlCreateAPIView.as_view(), name='create-url'),
     path('edit-url/<int:pk>/' ,ShortUrlUpdatedAPIView.as_view() , name='edit-url' ),
     path('delete-url/<int:pk>/' ,ShortUrlDeleteAPIView.as_view(), name='delete-url' ),
     path('list-url/',ShortUrlListAPIView.as_view(), name='list-url'),
+    path('<str:short_code>/', ShortURLRedirectAPIView.as_view(), name='short-url-redirect')
+
 ]
--- links/api/v1/views.py
+++ links/api/v1/views.py
@@ -1,10 +1,33 @@
 from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
+from django.shortcuts import get_object_or_404 , redirect
+from django.utils import timezone
+from django.core.files.base import ContentFile
 
+import redis
 from .serializers import ShortUrlCreateSerializer , ShortUrlUpdateSerializer , ShortUrlListSerializer
-from ...models import ShortURL
+from ...models import ShortURL,ClickStats
+import qrcode
+from io import BytesIO
 
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+def generate_qr_code(link):
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    filename = f"{link}.png"
+    filebuffer = ContentFile(buffer.getvalue())
+    return filename, filebuffer
 
 class ShortUrlCreateAPIView(CreateAPIView):
     serializer_class = ShortUrlCreateSerializer
@@ -30,3 +53,38 @@ class ShortUrlListAPIView(ListAPIView):
     permission_classes = [IsAuthenticated]
     def get_queryset(self):
         return ShortURL.objects.filter(user=self.request.user)
+
+class ShortURLRedirectAPIView(APIView):
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            return x_forwarded_for.split(',')[0]
+        return request.META.get('REMOTE_ADDR')
+
+    def get(self, request, short_code, format=None):
+        link = None
+        original_url = r.get(short_code)
+        if original_url:
+            original_url = original_url.decode('utf-8')
+            link = get_object_or_404(ShortURL, short_code=short_code, is_active=True)
+        else:
+            link = get_object_or_404(ShortURL, short_code=short_code, is_active=True)
+            original_url = link.original_url
+            r.set(short_code, original_url, ex=3600)
+
+        ip_address = self.get_client_ip(request) 
+        browser = request.META.get('HTTP_USER_AGENT', 'unknown')
+
+        ClickStats.objects.create(
+            short_url=link,
+            ip_address=ip_address,
+            browser=browser,
+            timestamp=timezone.now()
+        )
+
+        link.clicks += 1
+        link.last_clicked = timezone.now()
+        link.save(update_fields=['clicks', 'last_clicked'])
+
+        return redirect(original_url)
